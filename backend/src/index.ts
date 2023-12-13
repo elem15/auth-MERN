4d999f32051dc5dd49a3a896edee49cea0e556cd
Elem <elem15@yandex.ru>
2023-12-13 23:17:45 +0300

    feat: add base server

---
+++ backend/src/index.ts
@@ -0,0 +1,13 @@
+import express from 'express'
+
+const PORT = process.env.PORT || 5000
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello, world!")
+})
+
+app.listen(PORT, () => {
+  console.log('Server start on port: ' + PORT)
+})
