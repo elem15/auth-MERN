3787c22ea2019ee3fee8740ff99d202a50365716
Elem <elem15@yandex.ru>
2023-12-14 18:25:00 +0300

    feat: connect to mongoDB

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,9 @@
+import 'dotenv/config'
 import express from 'express'
+import mongoose from 'mongoose'
+import validateEnv from './utils/validateEnv'
 
-const PORT = process.env.PORT || 5000
+const PORT = validateEnv.PORT
 
 const app = express();
 
@@ -8,6 +11,9 @@ app.get("/", (req, res) => {
   res.send("Hello, world!")
 })
 
-app.listen(PORT, () => {
-  console.log('Server start on port: localhost:' + PORT)
-})
+mongoose.connect(validateEnv.MONGO_CONNECTION_STRING).then(() => {
+  console.log('Connected');
+  app.listen(PORT, () => {
+    console.log('Server start on port: localhost:' + PORT)
+  })
+}).catch((e) => console.error(e))
