287f11f90112eca32df2e5674aeedc46fb84f50a
Elem <elem15@yandex.ru>
2023-12-14 18:57:57 +0300

    refactor: delete dist folder from git

--- backend/dist/utils/validateEnv.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const envalid_1 = require("envalid");
-exports.default = (0, envalid_1.cleanEnv)(process.env, {
-    MONGO_CONNECTION_STRING: (0, envalid_1.str)(),
-    PORT: (0, envalid_1.port)(),
-});
