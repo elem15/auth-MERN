17d5b2a264f07e5eab12ca5c002f857a8367e2bc
Elem <elem15@yandex.ru>
2023-12-14 18:34:36 +0300

    feat: display connection

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -3,12 +3,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const PORT = process.env.PORT || 5000;
-const app = (0, express_1.default)();
-app.get("/", (req, res) => {
-    res.send("Hello, world!");
-});
-app.listen(PORT, () => {
-    console.log('Server start on port: localhost:' + PORT);
-});
+const app_1 = __importDefault(require("./app"));
+const mongoose_1 = __importDefault(require("mongoose"));
+const validateEnv_1 = __importDefault(require("./utils/validateEnv"));
+const PORT = validateEnv_1.default.PORT;
+mongoose_1.default.connect(validateEnv_1.default.MONGO_CONNECTION_STRING).then(() => {
+    console.log('Connected');
+    app_1.default.get("/net", (req, res) => {
+        res.send("Connected");
+    });
+    app_1.default.listen(PORT, () => {
+        console.log('Server start on port: localhost:' + PORT);
+    });
+}).catch((e) => console.error(e));
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,18 +1,14 @@
-import 'dotenv/config'
-import express from 'express'
-import mongoose from 'mongoose'
+import app from './app';
+import mongoose from 'mongoose';
 import validateEnv from './utils/validateEnv'
 
 const PORT = validateEnv.PORT
 
-const app = express();
-
-app.get("/", (req, res) => {
-  res.send("Hello, world!")
-})
-
 mongoose.connect(validateEnv.MONGO_CONNECTION_STRING).then(() => {
   console.log('Connected');
+  app.get("/net", (req, res) => {
+    res.send("Connected")
+  })
   app.listen(PORT, () => {
     console.log('Server start on port: localhost:' + PORT)
   })
