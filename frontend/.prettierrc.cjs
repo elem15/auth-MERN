ddfaa61790fdf1765392bdc71eaec9704a320dd4
Elem <elem15@yandex.ru>
2023-12-15 19:43:15 +0300

    feat: packages installation

---
+++ frontend/.prettierrc.cjs
@@ -0,0 +1,10 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+  "trailingComma": "all",
+  "tabWidth": 2,
+  "semi": false,
+  "singleQuote": true,
+  "printWidth": 120,
+  "bracketSpacing": true,
+  "endOfLine": "lf"
+}
---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  const f = 'fdasf'
+
+  return <div>    fdasf
+    <div>
+      fdasf
+      {f}
+
+    </div>
+  </div>
+
+}
+
+export default App
