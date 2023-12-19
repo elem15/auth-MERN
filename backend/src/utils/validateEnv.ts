e24b60a5008413cf25d359ef4b30a3c99c1272a7
Elem <elem15@yandex.ru>
2023-12-19 17:19:16 +0300

    deploy: fix - remove unused env

--- backend/src/utils/validateEnv.ts
+++ backend/src/utils/validateEnv.ts
@@ -2,7 +2,6 @@ import { cleanEnv, port, str } from 'envalid'
 
 export default cleanEnv(process.env, {
   MONGO_CONNECTION_STRING: str(),
-  MONGO_CONNECTION_IMAGES: str(),
   SESSION_SECRET: str(),
   PORT: port(),
 })
