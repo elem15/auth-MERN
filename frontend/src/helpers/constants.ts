f523bbce74d10ad3c7faaa41148cd8f28b074c89
Elem <elem15@yandex.ru>
2023-12-21 01:49:02 +0300

    fix: remote url

--- frontend/src/helpers/constants.ts
+++ frontend/src/helpers/constants.ts
@@ -1,3 +1,3 @@
 export const REMOTE_URL = 'https://auth-mern-0pty.onrender.com'
 export const BASE_URL = 
-  process.env.NODE_ENV === 'development' ? 'http://localhost:5000' : process.env.HOSTNAME
+  process.env.NODE_ENV === 'development' ? 'http://localhost:5000' : REMOTE_URL
