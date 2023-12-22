f44f819577e3b9101773310724cd56177419b379
Elem <elem15@yandex.ru>
2023-12-22 16:25:47 +0300

    fix: BASE_URL from vite import.meta.env

--- frontend/src/helpers/constants.ts
+++ frontend/src/helpers/constants.ts
@@ -1,3 +1,2 @@
 export const REMOTE_URL = 'https://auth-mern-0pty.onrender.com'
-export const BASE_URL = 'http://localhost:5000' 
-  // process.env.NODE_ENV === 'development' ? 'http://localhost:5000' : REMOTE_URL
+export const BASE_URL = import.meta.env.VITE_LOCAL_DEV ? 'http://localhost:5000' : 'https://auth-mern-0pty.onrender.com'
