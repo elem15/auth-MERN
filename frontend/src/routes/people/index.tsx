415f363e1747b855226bc6a06c23e1df48731077
Elem <elem15@yandex.ru>
2023-12-25 01:37:54 +0300

    feat: add scroll-watcher to people

--- frontend/src/routes/people/index.tsx
+++ frontend/src/routes/people/index.tsx
@@ -1,10 +1,13 @@
 import { useEffect } from 'react'
+import { useNavigate } from 'react-router'
+import { toast } from 'react-custom-alert'
+
 import Preloader from '../../components/loader/Preloader'
 import { useGetUsersQuery } from '../../services/usersApi'
-import { useNavigate } from 'react-router'
 import { Card } from '../../components/card/Card'
 import { Image } from '../../components/image/Image'
-import { toast } from 'react-custom-alert'
+
+import s from './index.module.css'
 
 export const People = () => {
   const navigate = useNavigate()
@@ -22,12 +25,14 @@ export const People = () => {
       {isLoading && <Preloader />}
 
       {data && <div className='mt-2 mx-auto'>
+        <div className={s.scrollWatcher}></div>
         <h1 className='text-center text-2xl font-semibold'>All users except you</h1>
 
         <ul className='flex flex-wrap justify-center'>{data.map(user => (
           <li key={user._id}>
             <Card>
-              <Image src={user.img} alt={user.name} width={256} height={256} isLoading={isLoading} className='group-hover:scale-[101%]' />
+              <Image src={user.img} alt={user.name} width={256} height={256}
+                isLoading={isLoading} className='group-hover:scale-[101%]' />
               <h2 className='text-lg'>Name: {user.name}</h2>
               <p className='text-lg w-full'>Age: {user.age} year(s)</p>
             </Card>
